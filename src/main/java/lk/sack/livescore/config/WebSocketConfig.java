f0e495d5db20892e1010e593ac1982dfebbab6e0
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2020-03-15 20:31:04 +0530

    Allow all origines to the stomp endpoint (#17)

--- src/main/java/lk/sack/livescore/config/WebSocketConfig.java
+++ src/main/java/lk/sack/livescore/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package lk.sack.livescore.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/live").withSockJS();
+        registry.addEndpoint("/live").setAllowedOrigins("*").withSockJS();
     }
 
 }
